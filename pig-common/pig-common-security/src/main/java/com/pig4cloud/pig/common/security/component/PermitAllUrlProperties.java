74bb267788f3433be6c6cc26363d675d7fa66a49
Peter Sun <hellosunxuejian@gmail.com>
2022-04-16 00:14:10 +0800

    :localize:配置修改成自己环境的

--- pig-common/pig-common-security/src/main/java/com/pig4cloud/pig/common/security/component/PermitAllUrlProperties.java
+++ pig-common/pig-common-security/src/main/java/com/pig4cloud/pig/common/security/component/PermitAllUrlProperties.java
@@ -42,7 +42,7 @@ import java.util.regex.Pattern;
  * @author lengleng
  * @date 2020-03-11
  * <p>
- * 资源服务器对外直接暴露URL,如果设置contex-path 要特殊处理
+ * 资源服务器对外直接暴露URL,如果设置context-path 要特殊处理
  */
 @Slf4j
 @ConfigurationProperties(prefix = "security.oauth2.ignore")
